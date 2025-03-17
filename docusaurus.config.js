3add527f86d2c4b40526539d61fbd2b8d44dba9e
Antoine COULON <a.antoinecoulon@gmail.com>
2025-03-17 09:02:58 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,7 +57,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/antoinecoulon/antoinecoulon-docs',
+            'https://github.com/antoinecoulon/antoinecoulon-docs/tree/master/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
